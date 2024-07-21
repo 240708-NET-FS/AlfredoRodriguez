3dd471b5584a84d08a2dbbd0f3ccec1d3421bc98
dmo575 <dmo5@protonmail.com>
2024-07-20 23:20:16 -0400

    Finished implementing bare-bones text editor

--- project-1/ConsoleApplication/Utils/TextEditor.cs
+++ project-1/ConsoleApplication/Utils/TextEditor.cs
@@ -1,17 +1,14 @@
 using System.Net;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 
 public class TextEditor
 {
     public String Title { get; set; } = null!;
     private List<char> Text = new List<char>();
-    //private (int x, int y) ContainerOffset = (0,1);
-    private (int x, int y) ContainerOffset = (5,5);
-
-
-    // WOW, I just tried doing => to generate the DynamicContainer's origin at runtime with the ContainerOffset and it just worked.
-    private ((int x, int y) origin, (int w, int h) dimensions) DynamicContainer = ((0, 0),(Console.WindowWidth - 5 - /* 1 */ 200,Console.WindowHeight - 5 - 3));
-
+    private (int x, int y) ContainerOffset = (1,1);
+    private (int w, int h) DynamicContainer;
+    private int TextVerticalOffset = 0;
 
     public TextEditor(String title, String text)
     {
@@ -30,72 +27,83 @@ public class TextEditor
     {
         Console.Clear();
 
-        // Print the text inside the text container space.
-        PrintText();
-
-        // Place the cursor inside the text container space, at 0,0.
+        // Set cursor's initial position.
         Console.SetCursorPosition(ContainerOffset.x, ContainerOffset.y);
 
+        // Do initial print.
+        UpdateContainerSize();
+        PrintText();
+
+        // Run as long as we dont hit the Escape key.
         for(ConsoleKeyInfo input = Console.ReadKey(true); input.Key != ConsoleKey.Escape; input = Console.ReadKey(true))
         {
-            //System.Console.WriteLine("You just typed: " + input.Key);
+            // Updates the container's dimensions based on the current state of the console.
             UpdateContainerSize();
-            ParseInput(input);
+
+            // Process input.
+            ProcessInput(input);
+
+            // Print the latest state of the text.
+            PrintText();
         }
     }
 
     private void UpdateContainerSize()
     {
+        /* (int w, int h) oldValues = DynamicContainer;
+        int hoveredCharIndex = FromContainerToArrayIndex(FromConsoleToContainer(Console.GetCursorPosition())); */
         // This keeps the container update after a console window's resize.
-        DynamicContainer = (ContainerOffset, (Console.WindowWidth - ContainerOffset.x - /* 1 */ 200,Console.WindowHeight - ContainerOffset.y - 3));
+        DynamicContainer = (40, 10);
+
+/*         // The console has changed shapes, so we will reposition the cursor based on its last position on the text array.
+        if(DynamicContainer.w != oldValues.w || DynamicContainer.h != oldValues.h)
+        {
+
+            // find the column where our cursor should be standing at given the new container dimensions.
+            int hoveredRowIndex = hoveredCharIndex / DynamicContainer.w;
+
+            // If that column is hidden up from view due scroll, scroll up to it.
+            if(TextVerticalOffset > hoveredRowIndex) TextVerticalOffset = hoveredRowIndex;
+
+            // If that column is hidden down from view due scroll, scroll down to it.
+            if(hoveredRowIndex > TextVerticalOffset + DynamicContainer.h) TextVerticalOffset -= hoveredRowIndex - (TextVerticalOffset + DynamicContainer.h);
+
+            // Position the cursor to hover over the desired char again.
+            (int x, int y) newPos = FromArrayToContainer(hoveredCharIndex);
+            Console.SetCursorPosition(newPos.x, newPos.y);
+        } */
     }
 
     // Parse input and call appropiate methods
-    private void ParseInput(ConsoleKeyInfo keyInfo)
+    private void ProcessInput(ConsoleKeyInfo keyInfo)
     {
-
-        // ==========================================================================
-        // NOTE: Make sure the cursor cannot travel over the boundaries of the list.
-        // OR: Make the list adapt to wathever the cursor wants to do there, maybe
-        // make the list add a bunch of empty spaces plus wathever key....
-        // That reminds me... shifting only if text visible. How does an Enter evn...
-        // Maybe an enter means to make empty spaces of wathever is left of the line, plus one space.
-        // Then better to not let the cursor get out of bounds from the array.
-        // ==========================================================================
-
         switch (keyInfo.Key)
         {
-            case ConsoleKey.LeftArrow:
-            case ConsoleKey.RightArrow:
-            case ConsoleKey.DownArrow:
-            case ConsoleKey.UpArrow:
-                MoveCursor(keyInfo.Key);
+            // We will not implement enter fow now.
+            case ConsoleKey.Enter:
                 return;
-            case ConsoleKey.Spacebar:
-
-            break;
+            case ConsoleKey.UpArrow:
+            case ConsoleKey.DownArrow:
+            case ConsoleKey.RightArrow:
+            case ConsoleKey.LeftArrow:
+                MoveCursor(keyInfo.Key);
+                break;
+            case ConsoleKey.Backspace:
+                RemoveCharacter();
+                break;
+            default:
+                AddCharacter(keyInfo.KeyChar);
+                break;
         }
-        char key = keyInfo.KeyChar;
-        // Add Key to the text.
     }
 
-    private void SelectKey()
-    {
-
-    }
-
-    private void GetCharAtConsolePos(int x, int y)
-    {
-        // console dimensions, we take achunk off there and stablish our rect.
-        // When printing, we always display based on the 
-    }
-
+    // Defines to move over the text via the arrow keys.
     private void MoveCursor(ConsoleKey direction)
     {
-        // Cursor position on the console
+        // Cursor position on the console.
         (int x, int y) pos = Console.GetCursorPosition();
 
-        // Transform the position to cursor's position on the container
+        // Transform the position to cursor's position on the container.
         pos.x -= ContainerOffset.x;
         pos.y -= ContainerOffset.y;
 
@@ -106,10 +114,10 @@ public class TextEditor
         switch (direction)
         {
             case ConsoleKey.UpArrow:
+            // If moving up makes the cursor go OOB:
             if(pos.y - 1 < 0)
             {
                 // OOB
-
                 newPosContainerSpace = pos;
                 MoveText(-1);
             }
@@ -119,10 +127,10 @@ public class TextEditor
             break;
 
             case ConsoleKey.DownArrow:
-            if(pos.y + 1 >= DynamicContainer.dimensions.h)
+            // If moving down makes the cursor go OOB:
+            if(pos.y + 1 >= DynamicContainer.h)
             {
                 // OOB
-
                 newPosContainerSpace = pos;
                 MoveText(1);
             }
@@ -132,16 +140,18 @@ public class TextEditor
             break;
 
             case ConsoleKey.LeftArrow:
+            // If moving left makes the cursor go OOB:
             if(pos.x - 1 < 0)
             {
                 // Move cursor one level UP and far RIGHT
-                newPosContainerSpace = (DynamicContainer.dimensions.w - 1, pos.y - 1);
+                newPosContainerSpace = (DynamicContainer.w - 1, pos.y - 1);
 
                 // If new position's Y value is OOB
                 if(newPosContainerSpace.y < 0)
                 {
                     // Reposition to be inside the vertical boundaries.
-                    newPosContainerSpace = (0, 0);
+                    // "Position the cursor at (0, 0), unless we have text lines not rendered on top, on such a case position the cursor at (MAX, 0)"
+                    newPosContainerSpace = (TextVerticalOffset > 0 ? DynamicContainer.w - 1 : 0, 0);
                     // Ask the text to shift UPWARDS
                     MoveText(-1);
                 }
@@ -152,16 +162,17 @@ public class TextEditor
             break;
 
             case ConsoleKey.RightArrow:
-            if(pos.x + 1 >= DynamicContainer.dimensions.w)
+            // If moving right makes the cursor go OOB:
+            if(pos.x + 1 >= DynamicContainer.w)
             {
                 // Move cursor one level DOWN and far LEFT
                 newPosContainerSpace = (0, pos.y + 1);
 
                 // If new position's Y value is OOB
-                if(newPosContainerSpace.y >= DynamicContainer.dimensions.h)
+                if(newPosContainerSpace.y >= DynamicContainer.h)
                 {
                     // Reposition to be inside the vertical boundaries.
-                    newPosContainerSpace = (DynamicContainer.dimensions.w - 1, DynamicContainer.dimensions.h - 1);
+                    newPosContainerSpace = (0, DynamicContainer.h - 1);
                     // Ask the text to shift UPWARDS
                     MoveText(1);
                 }
@@ -174,33 +185,29 @@ public class TextEditor
 
 
         // Check if the final position is out of bounds of the text.
-        //int oneDimPos = newPosContainerSpace.y * DynamicContainer.dimensions.w + newPosContainerSpace.x + 1;
-        int oneDimPos = FromContainerToArray(newPosContainerSpace);
+        int cursorIndexPosOnArray = FromContainerToArrayIndex(newPosContainerSpace);
 
 
         // If thew new position would be outside of the text we have (+1), then we dont move. We need to create text.
-        if(oneDimPos > Text.Count + 1)
+        if(cursorIndexPosOnArray >= Text.Count)
         {
-            newPosContainerSpace = ArrayToContainer(Text.Count);
+            newPosContainerSpace = FromArrayToContainer(Text.Count - (TextVerticalOffset * DynamicContainer.w));
         }
 
 
         // Convert the final position in container space to console space and set the cursor position to it.
-        //Console.SetCursorPosition(newPosContainerSpace.x + ContainerOffset.x, newPosContainerSpace.y + ContainerOffset.y);
         (int x, int y) consoleSpace = FromContainerToConsole(newPosContainerSpace);
         Console.SetCursorPosition(consoleSpace.x, consoleSpace.y);
-
     }
 
-
-    private (int x, int y) ArrayToContainer(int index)
+    private (int x, int y) FromArrayToContainer(int index)
     {
-        return (index - (index / DynamicContainer.dimensions.w) * DynamicContainer.dimensions.w, index / DynamicContainer.dimensions.w);
+        return (index - (index / DynamicContainer.w) * DynamicContainer.w, index / DynamicContainer.w);
     }
 
     private (int x, int y) FromConsoleToContainer((int x, int y) consoleCoords)
     {
-        return (consoleCoords.x - DynamicContainer.origin.x, consoleCoords.y - DynamicContainer.origin.y);
+        return (consoleCoords.x - ContainerOffset.x, consoleCoords.y - ContainerOffset.y);
     }
 
     private (int x, int y) FromContainerToConsole((int x, int y) containerCoords)
@@ -208,46 +215,95 @@ public class TextEditor
         return (containerCoords.x + ContainerOffset.x, containerCoords.y + ContainerOffset.y);
     }
 
-    private int FromContainerToArray((int x, int y) containerCoords)
+    private int FromContainerToArrayIndex((int x, int y) containerCoords)
     {
-        return containerCoords.y * DynamicContainer.dimensions.w + containerCoords.x + 1;
+        return (TextVerticalOffset * DynamicContainer.w) + (containerCoords.y * DynamicContainer.w + containerCoords.x);
     }
 
-    private void Enter()
+    // given an array pos, return its row position in the text container (0 based).
+    private int FromArrayToRowIndex(int arrayPos)
     {
-        //An enter creates a new line, which in this single string means spli
-        int cursorPosOnArray = FromContainerToArray(FromConsoleToContainer(Console.GetCursorPosition()));
+        return arrayPos - (arrayPos / DynamicContainer.w) * DynamicContainer.w;
+    }
 
-        //Text.InsertRange()
+    private void AddCharacter(char charToAdd)
+    {
 
-        // Enter means add a new list.
-        // When parsing the list to text, take each new list as a \n
+        // Reject any null char.
+        if(charToAdd == '\0') return;
 
+        // Get the character index we are hovering at with the cursor.
+        int cursorIndexPosOnArray = FromContainerToArrayIndex(FromConsoleToContainer(Console.GetCursorPosition()));
 
+        // If we are just OOB, we need to add, not insert. And move the cursor to the right.
+        if(cursorIndexPosOnArray + 1 == Text.Count)
+        {
+            Text.Add(charToAdd);
+        }
+        else
+        {
+            Text.Insert(cursorIndexPosOnArray, charToAdd);
+        }
+
+        // Move the cursor to the right.
+        MoveCursor(ConsoleKey.RightArrow);
     }
 
 
+    private void RemoveCharacter()
+    {
+        // Get the character index we are hovering at with the cursor.
+        int cursorIndexPosOnArray = FromContainerToArrayIndex(FromConsoleToContainer(Console.GetCursorPosition()));
+
+        // Remove previous char, if it exists.
+        if(cursorIndexPosOnArray > 0)
+        {
+            Text.RemoveAt(cursorIndexPosOnArray - 1);
+            MoveCursor(ConsoleKey.LeftArrow);
+        }
+    }
+
+
+    // Properly updates the TextVerticalOffset variable which is used when displaying text on the container.
     private void MoveText(int shiftDirection)
     {
+        TextVerticalOffset += shiftDirection;
+
+        // 
+        if(TextVerticalOffset < 0) TextVerticalOffset = 0;
 
     }
 
+    // Given a container (DynamicContainer), an origin (ContainerOffset) and a vertical offset (TextVerticalOffset), it displays as much as the
+    // that it can fit on the container rectangle.
     public void PrintText()
     {
-        int w = DynamicContainer.dimensions.w;
+        if(Text.Count == 0) return;
+
+        // record cursor position before starting.
+        (int x, int y) pos = Console.GetCursorPosition();
+
+        // Clear the console to avoid ghosting.
+        Console.Clear();
+
+        // Container width.
+        int w = DynamicContainer.w;
 
         // For each line...
-        // It also runs if the line it not completely filled with characters: ((y_line + 1) % Text.Count == 0 ? 0 : 1)
-        for(int y_line = 0; y_line < (Text.Count / w) + ((y_line + 1) % Text.Count == 0 ? 0 : 1); y_line++)
+        // It also runs if the line it not completely filled with characters: ((y_row + 1) % Text.Count == 0 ? 0 : 1)
+        // Takes TextVerticalOffset into consideration.
+        for(int y_row = TextVerticalOffset; y_row < + (Text.Count / w) + ((y_row + 1) % Text.Count == 0 ? 0 : 1) && y_row - TextVerticalOffset < DynamicContainer.h; y_row++)
         {
-            Console.SetCursorPosition(ContainerOffset.x, ContainerOffset.y + y_line);
-            // For each char on that line...
-            for(int x_charIndex = 0; x_charIndex < w && (y_line * w) + x_charIndex < Text.Count; x_charIndex++)
-            {
-                System.Console.Write(Text[(y_line * w) + x_charIndex]);
-            }
+            Console.SetCursorPosition(ContainerOffset.x, ContainerOffset.y + y_row - TextVerticalOffset);
 
-            //System.Console.WriteLine();
+            // For each char on that row...
+            for(int x_charIndex = 0; x_charIndex < w && (y_row * w) + x_charIndex < Text.Count; x_charIndex++)
+            {
+                System.Console.Write(Text[(y_row * w) + x_charIndex]);
+            }
         }
+
+        // put cursor back in place.
+        Console.SetCursorPosition(pos.x, pos.y);
     }
 }
